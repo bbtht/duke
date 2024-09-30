5cdf23ecb3e89a82aa296f21ab0bbe8bf19724d5
Hui Ting <>
2024-10-01 01:34:46 +0800

    add level 0

---
+++ src/main/java/Sunny.java
@@ -0,0 +1,10 @@
+public class Sunny {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________\n");
+        System.out.println(" Hello! I'm Sunny");
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________\n");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________\n");
+    }
+}
